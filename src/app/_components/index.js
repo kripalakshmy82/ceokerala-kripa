445808e5ed5338d84405c4db4c092be76100ae42
helpgitt007 <anas@invis.in>
2023-08-01 11:17:50 +0530

    NewsList With Pagination

--- src/app/_components/index.js
+++ src/app/_components/index.js
@@ -1,11 +1,13 @@
 import AppButton from "./AppButton";
 import AvatarInfo from "./AvatarInfo";
+import AppPagination from "./AppPagination";
 import AppCard, { InfoCard } from "./AppCard";
 import { AppBox, AppText, AppSection } from "./Elements";
 
 export {
   AppButton,
   AvatarInfo,
+  AppPagination,
   AppCard,
   InfoCard,
   AppBox,
--- src/app/home/NewsUpdate/shared/NewsList.js
+++ src/app/home/NewsUpdate/shared/NewsList.js
@@ -4,53 +4,13 @@ import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import classNames from "classnames";
 
-import { AppBox, AppCard, AppText, AppButton } from "@/app/_components";
-
-// Use the default Pagination Component In react bootstrap
-// And change this in to an root component
-// url : https://react-bootstrap.netlify.app/docs/components/pagination
-function Pagination() {
-  return (
-    <div className="pagination-wrap">
-      <ul className="pagination justify-content-center m-0">
-        <li className="page-item disabled">
-          <a className="page-link border-0">
-            <i>
-              <img src="./icons/chevron-left.svg" alt="" />
-            </i>
-            Previous
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link border-0 text-center" href="#">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a
-            className="page-link page-link-active border-0 text-center text-white"
-            href="#"
-          >
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link border-0 text-center" href="#">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link border-0" href="#">
-            Next
-            <i>
-              <img src="./icons/chevron-right.svg" alt="" />
-            </i>
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
+import {
+  AppBox,
+  AppCard,
+  AppText,
+  AppButton,
+  AppPagination,
+} from "@/app/_components";
 
 const NewsListItem = ({
   title,
@@ -90,31 +50,14 @@ const NewsListItem = ({
 };
 
 const NewsList = ({ activeKey, data }) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-    return () => {
-      setLoading(true);
-    };
-  }, [activeKey]);
-
   return (
     <>
-      {loading ? (
-        <AppBox className="d-flex align-items-center justify-content-center">
-          <Spinner animation="border" size="lg" />
+      {data.map((news, key) => (
+        <AppBox className="mb-3" key={key}>
+          <NewsListItem {...news} />
         </AppBox>
-      ) : (
-        <>
-          {data.map((news, key) => (
-            <AppBox className="mb-3" key={key}>
-              <NewsListItem {...news} />
-            </AppBox>
-          ))}
-          <Pagination />
-        </>
-      )}
+      ))}
+      <AppPagination />
     </>
   );
 };
